e35c22c9feea946833473bf907746b1f2948adfc
gabriel <gabrieldossantosvargas@gmail.com>
2020-02-19 22:55:01 -0300

    Inserindo composer PHPMailer

---
+++ src/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+define("MAIL", [
+    "host" => "",
+    "port" => "",
+    "username" => "",
+    "passwd" => "",
+    "from_name" => "Gabriel Vargas",
+    "from_email" => "gabrieldossantosvargas@gmail.com"
+
+]);
